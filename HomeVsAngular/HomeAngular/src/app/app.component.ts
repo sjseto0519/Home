3eaa8ecd7e5bbbc397d0fcefffad213c3b2e3995
Scott Seto <sjseto0519@gmail.com>
2022-06-26 16:39:58 -0400

    Update resume

--- HomeVsAngular/HomeAngular/src/app/app.component.ts
+++ HomeVsAngular/HomeAngular/src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Title }     from '@angular/platform-browser';
 import resumeObj from '../assets/json/resume.json';
-import Partition from './models/Partition';
-import ExternalLink from './models/ExternalLink';
 
 @Component({
   selector: 'app-root',
